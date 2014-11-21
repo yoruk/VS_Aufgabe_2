e731708e929ddc104dfa78e87d2c0778b7ee4dc2
Michael Schmidt <michael.schmidt1@haw-hamburg.de>
2014-11-22 00:32:53 +0100

    ei wneig code in main.cpp auskommentiert

--- vslab/src/main.cpp
+++ vslab/src/main.cpp
@@ -11,26 +11,30 @@
 #include <iostream>
 #include <algorithm>
 
-#include <boost/multiprecision/cpp_int.hpp>
-#include <boost/multiprecision/miller_rabin.hpp>
+//#include <boost/multiprecision/cpp_int.hpp>
+//#include <boost/multiprecision/miller_rabin.hpp>
 
 #include "caf/all.hpp"
 #include "caf/io/all.hpp"
 
-using std::cout;
-using std::endl;
-using std::vector;
-using std::string;
+//using std::cout;
+//using std::endl;
+//using std::vector;
+//using std::string;
 
-using boost::multiprecision::int512_t;
-using boost::multiprecision::miller_rabin_test;
+//using boost::multiprecision::int512_t;
+//using boost::multiprecision::miller_rabin_test;
 
+using namespace std;
 using namespace caf;
 
+/*
 inline bool is_probable_prime(const int512_t& value) {
   // increase 25 to a higher value for higher accuracy
   return miller_rabin_test(value, 25);
 }
+*/
+
 
 void run_server(long port) {
   cout << "run_server: implement me" << endl;
@@ -39,6 +43,7 @@ void run_server(long port) {
 void run_manager(long workers, const string& host, long port) {
   cout << "run_manager: implement me" << endl;
 }
+
 void run_client(const string& host, long port) {
   cout << "run_client: implement me" << endl;
 }
@@ -53,6 +58,7 @@ optional<long> long_arg(const string& arg) {
   return none;
 }
 
+/*
 // tell CAF how to serialize int512_t
 class int512_t_uti : public caf::detail::abstract_uniform_type_info<int512_t> {
  protected:
@@ -66,25 +72,31 @@ class int512_t_uti : public caf::detail::abstract_uniform_type_info<int512_t> {
     iss >> *reinterpret_cast<int512_t*>(vptr);
   }
 };
+*/
 
 
 int main(int argc, char** argv) {
+
   // allow int512_t and vector<int512_t> to be used in messages
-  announce(typeid(int512_t), uniform_type_info_ptr{new int512_t_uti});
-  announce<vector<int512_t>>();
+  //announce(typeid(int512_t), uniform_type_info_ptr{new int512_t_uti});
+  //announce<vector<int512_t>>();
+
   // parse command line arguments
-  message_builder{argv + 1, argv + argc}.apply({
-    on("-s", long_arg) >> run_server,
-    on("-m", long_arg, val<string>, long_arg) >> run_manager,
-    on("-c", val<string>, long_arg) >> run_client,
-    others() >> [] {
-      cout << "usage:" << endl
-           << "  server:  -s <PORT>" << endl
-           << "  manager: -m <NUM_WORKERS> <SERVER_HOST> <SERVER_PORT>" << endl
-           << "  client:  -c <SERVER_HOST> <SERVER_PORT>" << endl
-           << endl;
-    }
-  });
-  await_all_actors_done();
-  shutdown();
+	message_builder{argv + 1, argv + argc}.apply({
+		on("-s", long_arg) >> run_server,
+		on("-m", long_arg, val<string>, long_arg) >> run_manager,
+		on("-c", val<string>, long_arg) >> run_client,
+		others() >> [] {
+			cout << "usage:" << endl
+			<< "  server:  -s <PORT>" << endl
+			<< "  manager: -m <NUM_WORKERS> <SERVER_HOST> <SERVER_PORT>" << endl
+			<< "  client:  -c <SERVER_HOST> <SERVER_PORT>" << endl
+			<< endl;
+		}
+	});
+
+	await_all_actors_done();
+	shutdown();
+
+	return 0;
 }
